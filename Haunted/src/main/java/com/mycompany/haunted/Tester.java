bb1b865ec8f0535ae1f9c2d600e7f1dbcb711cac
abrahamsj <90579607+abrahamsj@users.noreply.github.com>
2023-11-02 12:10:01 -0400

    Update Tester.java
    removing personal information

--- Haunted/src/main/java/com/mycompany/haunted/Tester.java
+++ Haunted/src/main/java/com/mycompany/haunted/Tester.java
@@ -1,12 +1,4 @@
-//********************************************************************************
-// NAME:JANEEL ABRAHAMS
-//PANTHERID:  [6242670]
-// CLASS: COP 2210 – [FALL 2020]
-// ASSIGNMENT #3 
-// DATE: [11/4/2020]
-//
-// I hereby swear and affirm that this work is solely my own, and not the work 
-// or the derivative of the work of someone else.
+
 
 package com.mycompany.haunted;
 
